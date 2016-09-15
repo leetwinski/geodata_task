83f38a9665ce5645dd74b34f62e261c48565634f
leetwin <leetwinski@gmail.com>
2016-09-15 13:45:15 +0300

    updated cache test

--- test/cache_test.clj
+++ test/cache_test.clj
@@ -6,29 +6,35 @@
 (repo/init-db)
 
 (def ^:private records
-  [["aaa" "aaa-addr1" 0.0 1.0 :timestamp (Date. 2010 1 1)]
+  [["aaa" "aaa-addr1" 0.0 1.0 :timestamp (Date. 110 1 1)]
    ["aaa" "aaa-addr2" 2.0 3.0]
    ["aaa" "aaa-addr2" 2.0 3.0]
-   ["bbb" "bbb-addr1" 4.0 5.0]])
+   ["bbb" "bbb-addr1" 4.0 5.0]
+   ["bbb" "bbb-addr2" 6.0 7.0 :timestamp (Date. 111 1 1)]])
 
 (doseq [data records]
   (apply repo/add-item data))
 
 (t/deftest cache-repo-test
-  (t/testing "items added to db"
+  (t/testing "items insertion"
     (let [db-records (repo/select-all)]
-      (t/is (= (count db-records) 4))
+      (t/is (= (count db-records) 5))
       
-      (t/is (= records
+      (t/is (= (map (partial take 4) records)
                (map (juxt :query_address :address :lat :lng)
                     db-records)))))
 
   (t/testing "repo lookup"
     (let [db-records (repo/get-items "aaa")]
       
-      (t/is (= (count db-records) 2))
+      (t/is (= (count db-records) 1))
 
-      (t/is (= (distinct
-                (keep #(when (= (first %) "aaa") (rest %)) records))
-               (map (juxt :address :lat :lng) db-records))))))
+      (t/is (= [["aaa-addr2" 2.0 3.0]]
+               (map (juxt :address :lat :lng) db-records)))))
+
+  (t/testing "outdated records deletion"
+    (do
+      (repo/remove-outdated)
+      (t/is (= (count (repo/select-all))
+               3)))))
 
